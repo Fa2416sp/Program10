5576ab4217252adcf149e56ab286e2a288b2cabd
alex tabota <alex-tabota@yandex.ru>
2021-01-24 19:44:31 +0300

    Добавьте файлы проекта.

---
+++ Program10/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace Program10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.системаКоординатToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.фигураToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дугиИСекторыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.путьКонтурToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.простоКонтурToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hQКонтурToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.заливкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сплошнаяЗаливкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.узорнаяЗаливкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.градиентнаяЗалливкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.фигураToolStripMenuItem,
+            this.заливкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(527, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 800);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.системаКоординатToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // системаКоординатToolStripMenuItem
+            // 
+            this.системаКоординатToolStripMenuItem.Name = "системаКоординатToolStripMenuItem";
+            this.системаКоординатToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.системаКоординатToolStripMenuItem.Text = "Система координат";
+            this.системаКоординатToolStripMenuItem.Click += new System.EventHandler(this.системаКоординатToolStripMenuItem_Click);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // фигураToolStripMenuItem
+            // 
+            this.фигураToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.дугиИСекторыToolStripMenuItem,
+            this.путьКонтурToolStripMenuItem,
+            this.простоКонтурToolStripMenuItem,
+            this.hQКонтурToolStripMenuItem});
+            this.фигураToolStripMenuItem.Name = "фигураToolStripMenuItem";
+            this.фигураToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.фигураToolStripMenuItem.Text = "Фигура";
+            // 
+            // дугиИСекторыToolStripMenuItem
+            // 
+            this.дугиИСекторыToolStripMenuItem.Name = "дугиИСекторыToolStripMenuItem";
+            this.дугиИСекторыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.дугиИСекторыToolStripMenuItem.Text = "Дуги и секторы";
+            this.дугиИСекторыToolStripMenuItem.Click += new System.EventHandler(this.дугиИСекторыToolStripMenuItem_Click);
+            // 
+            // путьКонтурToolStripMenuItem
+            // 
+            this.путьКонтурToolStripMenuItem.Name = "путьКонтурToolStripMenuItem";
+            this.путьКонтурToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.путьКонтурToolStripMenuItem.Text = "Путь- Контур ";
+            this.путьКонтурToolStripMenuItem.Click += new System.EventHandler(this.путьКонтурToolStripMenuItem_Click);
+            // 
+            // простоКонтурToolStripMenuItem
+            // 
+            this.простоКонтурToolStripMenuItem.Name = "простоКонтурToolStripMenuItem";
+            this.простоКонтурToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.простоКонтурToolStripMenuItem.Text = "Просто Контур";
+            this.простоКонтурToolStripMenuItem.Click += new System.EventHandler(this.простоКонтурToolStripMenuItem_Click);
+            // 
+            // hQКонтурToolStripMenuItem
+            // 
+            this.hQКонтурToolStripMenuItem.Name = "hQКонтурToolStripMenuItem";
+            this.hQКонтурToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hQКонтурToolStripMenuItem.Text = "HQ Контур";
+            this.hQКонтурToolStripMenuItem.Click += new System.EventHandler(this.hQКонтурToolStripMenuItem_Click);
+            // 
+            // заливкаToolStripMenuItem
+            // 
+            this.заливкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сплошнаяЗаливкаToolStripMenuItem,
+            this.узорнаяЗаливкаToolStripMenuItem,
+            this.градиентнаяЗалливкаToolStripMenuItem});
+            this.заливкаToolStripMenuItem.Name = "заливкаToolStripMenuItem";
+            this.заливкаToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.заливкаToolStripMenuItem.Text = "Заливка";
+            // 
+            // сплошнаяЗаливкаToolStripMenuItem
+            // 
+            this.сплошнаяЗаливкаToolStripMenuItem.Name = "сплошнаяЗаливкаToolStripMenuItem";
+            this.сплошнаяЗаливкаToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.сплошнаяЗаливкаToolStripMenuItem.Text = "Сплошная заливка";
+            this.сплошнаяЗаливкаToolStripMenuItem.Click += new System.EventHandler(this.сплошнаяЗаливкаToolStripMenuItem_Click);
+            // 
+            // узорнаяЗаливкаToolStripMenuItem
+            // 
+            this.узорнаяЗаливкаToolStripMenuItem.Name = "узорнаяЗаливкаToolStripMenuItem";
+            this.узорнаяЗаливкаToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.узорнаяЗаливкаToolStripMenuItem.Text = "Узорная заливка";
+            this.узорнаяЗаливкаToolStripMenuItem.Click += new System.EventHandler(this.узорнаяЗаливкаToolStripMenuItem_Click);
+            // 
+            // градиентнаяЗалливкаToolStripMenuItem
+            // 
+            this.градиентнаяЗалливкаToolStripMenuItem.Name = "градиентнаяЗалливкаToolStripMenuItem";
+            this.градиентнаяЗалливкаToolStripMenuItem.Size = new System.Drawing.Size(196, 22);
+            this.градиентнаяЗалливкаToolStripMenuItem.Text = "Градиентная залливка";
+            this.градиентнаяЗалливкаToolStripMenuItem.Click += new System.EventHandler(this.градиентнаяЗалливкаToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(527, 836);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem системаКоординатToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem фигураToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem дугиИСекторыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem путьКонтурToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem простоКонтурToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hQКонтурToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem заливкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сплошнаяЗаливкаToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem узорнаяЗаливкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem градиентнаяЗалливкаToolStripMenuItem;
+    }
+}
+
---
+++ Program10/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Program10
+{
+    public partial class Form1 : Form
+    {
+        Graphics Graph1;
+        Bitmap image1 = new Bitmap(500, 800);
+        Pen Pen1 = new Pen(Color.Black, 1);
+        Pen Pen2 = new Pen(Color.Brown, 2);
+        Pen Pen3 = new Pen(Color.Cyan, 4);
+        Pen Pen4 = new Pen(Color.Purple, 4);
+        Font drawFont = new Font("Arial", 12);
+        SolidBrush Brush1 = new SolidBrush(Color.Blue);
+        SolidBrush Brush2 = new SolidBrush(Color.LightGoldenrodYellow);
+        HatchBrush hatchbrush1 = new HatchBrush(HatchStyle.SolidDiamond, Color.Green);
+        int i = 0;
+        Point point1 = new Point(0, -200);
+        Point point2 = new Point(0, 200);
+        Point point3 = new Point(-200, 200);
+        Point point4 = new Point(0, 0);
+        Point point5 = new Point(-200, 0);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void системаКоординатToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.Clear(Color.White);
+            for (i = -250; i <= 250; i += 50)
+            {
+                Graph1.DrawString(Convert.ToString(i), drawFont, Brush1, i + 250, 410);
+            }
+            for (i = -400; i <= 400; i += 50)
+            {
+                Graph1.DrawString(Convert.ToString(i), drawFont, Brush1, 260, 400 - i);
+            }
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            Graph1.DrawLine(Pen1, -250, 0, 250, 0);
+            Graph1.DrawLine(Pen1, 0, -400, 0, 400);
+            for (int i = -250; i <= 250; i += 50)
+            {
+                Graph1.DrawEllipse(Pen3, i, 0, 1, 1);
+            }
+            for (int i = -400; i <= 400; i += 50)
+            {
+                Graph1.DrawEllipse(Pen3, 0, i, 1, 1);
+            }
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "BMP(*.bmp)|*.bmp";
+            saveFileDialog1.ShowDialog();
+            image1.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void дугиИСекторыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            for (i = 0; i <= 250; i += 10)
+                Graph1.DrawLine(Pen1, i, 400, i, 0);
+            for (i = 0; i <= 400; i += 10)
+                Graph1.DrawLine(Pen1, 0, i, 250, i);
+            Graph1.DrawArc(Pen3, 80, 80, 100, 100, 90, 45);
+            Graph1.DrawEllipse(Pen3, 130, 130, 1, 1);
+            Graph1.DrawPie(Pen4, 30, 40, 100, 100, 175, 110);
+        }
+
+        private void путьКонтурToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            GraphicsPath GP = new GraphicsPath();
+            GP.StartFigure();
+            GP.AddLine(-200, 0, -100, -100);
+            GP.AddLine(-100, -100, -100, 0);
+            GP.AddLine(-100, -100, 0, 0);
+            GP.AddLine(0, 0, 100, 0);
+            GP.CloseFigure();
+            GP.StartFigure();
+            GP.AddArc(0, 0, 200, 200, -90, -90);
+            GP.AddLine(0, 0, 0, 100);
+            GP.AddArc(-200, 0, 200, 200, 0, -90);
+            GP.CloseFigure();
+            Graph1.DrawPath(Pen2, GP);
+        }
+
+        private void простоКонтурToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            Pen3.Color = colorDialog1.Color;
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            Graph1.DrawLine(Pen3, -200, 0, -100, -100);
+            Graph1.DrawLine(Pen3, -100, -100, -100, 0);
+            Graph1.DrawLine(Pen3, -100, -100, 0, 0);
+            Graph1.DrawLine(Pen3, 0, 0, 100, 0);
+            Graph1.DrawArc(Pen3, 0, 0, 200, 200, -90, -90);
+            Graph1.DrawLine(Pen3, 0, 0, 0, 100);
+            Graph1.DrawArc(Pen3, -200, 0, 200, 200, 0, -90);
+            Graph1.DrawLine(Pen3, 0, 0, -200, 0);
+            
+        }
+
+        private void hQКонтурToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Pen3.DashStyle = DashStyle.Solid;
+            colorDialog1.ShowDialog();
+            Pen3.Color = colorDialog1.Color;
+            Graph1.PageUnit = GraphicsUnit.Pixel;
+            Graph1.SmoothingMode = SmoothingMode.HighQuality;
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            Graph1.DrawLine(Pen3, -200, 0, -100, -100);
+            Graph1.DrawLine(Pen3, -100, -100, -100, 0);
+            Graph1.DrawLine(Pen3, -100, -100, 0, 0);
+            Graph1.DrawLine(Pen3, 0, 0, 100, 0);
+            Graph1.DrawArc(Pen3, 0, 0, 200, 200, -90, -90);
+            Graph1.DrawLine(Pen3, 0, 0, 0, 100);
+            Graph1.DrawArc(Pen3, -200, 0, 200, 200, 0, -90);
+            Graph1.DrawLine(Pen3, 0, 0, -200, 0);
+        }
+
+        private void сплошнаяЗаливкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            GraphicsPath GP = new GraphicsPath();
+            GP.StartFigure();
+            GP.AddLine(-200, 0, -100, -100);
+            GP.AddLine(-100, -100, -100, 0);
+            GP.AddLine(-100, -100, 0, 0);
+            GP.AddLine(0, 0, 100, 0);
+            GP.CloseFigure();
+            GP.StartFigure();
+            GP.AddArc(0, 0, 200, 200, -90, -90);
+            GP.AddLine(0, 0, 0, 100);
+            GP.AddArc(-200, 0, 200, 200, 0, -90);
+            GP.CloseFigure();
+            Graph1.FillPath(Brush1, GP);
+        }
+
+        private void узорнаяЗаливкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            GraphicsPath GP = new GraphicsPath();
+            GP.StartFigure();
+            GP.AddLine(-200, 0, -100, -100);
+            GP.AddLine(-100, -100, -100, 0);
+            GP.AddLine(-100, -100, 0, 0);
+            GP.AddLine(0, 0, 100, 0);
+            GP.CloseFigure();
+            GP.StartFigure();
+            GP.AddArc(0, 0, 200, 200, -90, -90);
+            GP.AddLine(0, 0, 0, 100);
+            GP.AddArc(-200, 0, 200, 200, 0, -90);
+            GP.CloseFigure();
+            Graph1.FillPath(hatchbrush1, GP);
+        }
+
+        private void градиентнаяЗалливкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LinearGradientBrush LgBrush = new LinearGradientBrush(point3, point1, Color.FromArgb(255, 255, 0, 0), Color.FromArgb(255, 0, 0, 0));
+            Graph1 = Graphics.FromImage(image1);
+            pictureBox1.Image = image1;
+            Graph1.ScaleTransform(1, -1);
+            Graph1.TranslateTransform(250, -400);
+            GraphicsPath GP = new GraphicsPath();
+            GP.StartFigure();
+            GP.AddLine(-200, 0, -100, -100);
+            GP.AddLine(-100, -100, -100, 0);
+            GP.AddLine(-100, -100, 0, 0);
+            GP.AddLine(0, 0, 100, 0);
+            GP.CloseFigure();
+            GP.StartFigure();
+            GP.AddArc(0, 0, 200, 200, -90, -90);
+            GP.AddLine(0, 0, 0, 100);
+            GP.AddArc(-200, 0, 200, 200, 0, -90);
+            GP.CloseFigure();
+            Graph1.FillPath(LgBrush, GP);
+        }
+    }
+}
